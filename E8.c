9da0c5762a3f489c6a0bbc65358080c227fe90c5
Dineshs737 <dinesh77saarck@gmail.com>
2024-03-20 06:47:54 +0530

    add E8.c file

---
+++ E8.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+// double cal(int);
+int main()
+{
+    int user_num;
+    printf("----------------------------------------------\n");
+
+    printf("Scientific Calculator\n");
+
+    printf("---------------------------------------\n");
+
+    printf("1. Power\n");
+    printf("2. Square Root\n");
+    printf("3. Exponential\n");
+    printf("4. Log\n");
+    printf("5. Round\n");
+
+    printf("Enter the operation number :");
+    scanf("%d",&user_num);
+    printf("---------------------------------------\n");
+
+    // cal(user_num);
+
+
+    return 0;
+}
+
